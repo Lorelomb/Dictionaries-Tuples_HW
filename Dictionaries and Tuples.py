389c851e1c40fe5e2b2473c0281822b61573d557
Lorelomb <2020326@brunel.ac.uk>
2021-11-21 17:41:06 +0000

    Commit one

---
+++ Dictionaries and Tuples.py
@@ -0,0 +1,53 @@
+# Exercise 1.a
+# a_list = [3, 5, 6, 12]
+# bold_list = []
+# bold_list.append(a_list[0])
+# print("\033[1m",bold_list)
+
+# Exercise 1.b
+# a_list = [3, 5, 6, 12]
+# bold_list = []
+# bold_list.append(a_list[1:4])
+# print("\033[1m",bold_list)
+
+# Exercise 1.c
+# a_list = [3, 5, 6, 12]
+# print("\033[1m",a_list[::-1])
+
+# Exercise 1.d
+# a_list = [3, 5, 6, 12]
+# multiplied_list = [element * 3 for element in a_list]
+# print("\033[1m",multiplied_list)
+
+# Exercise 1.e
+# a_list = [3, 5, 6, 12]
+# even_list = []
+# for num in a_list:
+#     if (num % 2) == 0:
+#         even_list.append("True")
+#     else:
+#         even_list.append("False")
+# print("\033[1m",even_list)
+
+# Exercise 2
+# myList = ["axe","dagger","oranges"]
+# newList = []
+# for element in myList:
+#     newList.append("*")
+#     newList.append(element)
+#
+# print(newList)
+
+# Tuples Exercise 1
+# tupleList = [(), (), ('',), ('a', 'b'), ('a', 'b', 'c'), ('d')]
+# newTupleList = []
+# for element in tupleList:
+#     if element != ():
+#         newTupleList.append(element)
+# print(newTupleList)
+
+# Dictionaries Exercise 1
+
+
+# Dictionaries Exercise 2
+# room_items = {"item1": {"Name":"Lamp","Colour":"Red"},"item2":{"Name":"Table","Colour":"Brown","Type":0},"item3":{"Name":"Lamp","Colour":"Red"},"item4":{"Name":"Chair","Colour":"Green","Type":1}
